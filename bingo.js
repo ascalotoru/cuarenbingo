613b7fede36e9abf32963131ac1a1d51f0a438a0
root <root@vmi244077.contaboserver.net>
2020-04-26 19:46:58 +0200

    Añadidos comentarios. Lista de números. Check número repetido.

--- bingo.js
+++ bingo.js
@@ -1,26 +1,74 @@
-$("#frm_nuevo_numero").submit(function(event){
-    event.preventDefault();
-    var formData = {
-        'numero' : $('input[name=numero_actual]').val()
-    };
-    $.ajax({
-        type: 'POST',
-        url: 'bingo.php',
-        data: {datos: formData, action: 'nuevoNumero'},
-    }).done(function(data){
-        //console.log(parseInt(data.replace(/"/g,'')));
-        data = parseInt(data.replace(/"/g,''));
-        // $("#" + data).addClass("fuera");
-        pintarNumero(data);
-    });
+listaNumeros = [];
+$().ready(function(){
+    $("#frm_nuevo_numero").submit(function(event){
+        event.preventDefault();
+        var formData = {
+            'numero' : $('input[name=numero_actual]').val()
+        };
+        if(!revisarNumero){
+            $.ajax({
+                type: 'POST',
+                url: 'bingo.php',
+                data: {datos: formData, action: 'nuevoNumero'},
+            }).done(function(data){
+                data = parseInt(data.replace(/"/g,''));
+                getNumbers();
+                $("#insertar_numero").val("");
+                $("#insertar_numero").focus();
+            });
 
-    event.preventDefault();
+            event.preventDefault();
+        }else{
+            alert("Número incorrecto");
+        }
+    });
+    /* DEPRECATED
+    $("#refrescar").click(function(){
+        getNumbers();
+    });
+    */
+    getNumbers();
 });
 
-function leerFichero(){
 
-}
-
+/**
+ * Aplica la clase 'fuera' que simplemente cambia el fondo de la celda a negro
+ * y pone las letras en blanco
+ */
 function pintarNumero(numero){
     $("#" + numero).addClass("fuera");
+    if ($("#lista_numeros").text()){
+        $("#lista_numeros").text($("#lista_numeros").text() + ", " + numero);
+    }else{
+        $("#lista_numeros").text(numero);
+    }
+}
+
+/**
+ * Hace petición GET por AJAX y recibe el contenido del fichero con los números
+ * que ya han salido
+ */
+function getNumbers(){
+    $.ajax({
+        type: 'GET',
+        url: 'bingo.php',
+        data: {action: 'getNumbers'},
+        dataType: 'json'
+    }).done(function(data){
+        numeros = data;
+        $.each(data, function(index, value){
+            if(! value.length == 0){
+                pintarNumero(value);
+            }
+        });
+    });
+}
+
+function revisarNumero(numero){
+    $.each(listaNumeros, function(index, value){
+        if(value == numero){
+            return true;
+        }
+    });
+    return false;
 }
--- bingo.php
+++ bingo.php
@@ -2,7 +2,13 @@
     if(isset($_POST['action']) && !empty($_POST['action'])){
         $action = $_POST['action'];
         switch($action){
-            case 'nuevoNumero' : anadirNumero();
+            case 'nuevoNumero' : anadirNumero(); break;
+        }
+    }
+    if(isset($_GET['action']) && !empty($_GET['action'])){
+        $action = $_GET['action'];
+        switch($action){
+            case 'getNumbers': leerFichero(); break;
         }
     }
 
@@ -12,13 +18,18 @@
         while (! flock($fp, LOCK_EX)) { // Mientras no se pueda bloquear, esperar un segundo
             usleep(1000);
         }
-        fwrite($fp, $numero['numero'] . "\r\n");
-        flock ($fp, LOCK_UN);
+        fwrite($fp, $numero['numero'] . " ");
+        flock ($fp, LOCK_UN); // Desbloqueo el fichero
         fclose($fp);
         echo json_encode($numero['numero']);
     }
 
     function leerFichero(){
-        $
+        $fp = fopen('bingo.txt', 'r');
+        while (! flock($fp, LOCK_SH)){
+            usleep(1000);
+        }
+        $fichero = fread($fp, filesize('bingo.txt'));
+        echo json_encode(explode(' ', $fichero));
     }
 ?>
